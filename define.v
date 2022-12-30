88ef9468881b8fba804fd8acdd1a8156075066e5
蔡聿善 <jimmy012403976@gmail.com>
2022-12-30 10:07:42 +0800

    display finish and fix readImage

--- define.v
+++ define.v
@@ -2,12 +2,12 @@
 `define DEFINE
 	`define vgaTime 1
 	`define dotTime 2500
-	`define gameTime 60
+	`define gameTime 416666
 
 	`define screenwidth 640
 	`define screenheight 480
-	`define screenwidthnumlen 10
-	`define screenheightnumlen 10
+	`define screenwidthnumlen 30
+	`define screenheightnumlen 30
 
 	
 	
@@ -15,12 +15,21 @@
 	`define imagewidth 80
 	`define imageheight 40
 	
+	
 	`define datacount 10
+	
 	`define datatypelen 4
 	`define dataxlen 10
 	`define dataylen 10
 	`define datawidthlen 10
 	`define dataheigthlen 10
+	
+	`define datatypestart 0
+	`define dataxstart (`datatypestart+`datatypelen)
+	`define dataystart (`dataxstart+`dataxlen)
+	`define datawidthstart (`dataystart+`dataylen)
+	`define dataheigthstart (`datawidthstart+`datawidthlen)
+	`define datalen (`dataheigthstart+`dataheigthlen)
 	
 	`define scorelen 13
 `endif
--- readImage.v
+++ readImage.v
@@ -1,22 +1,35 @@
 `include "define.v"
 
-task readImage ;
-	output [`imagecount*`imagewidth*`imageheight-1:0] image;
-	reg [`imagewidth-1:0] tmp[0:`imageheight];
-	integer i;
-	integer k;
-	begin
-		for(i=0; i < `imagewidth; i=i+1)
-		begin
-			case(i)
-				0: $readmemb("./imageConverter/dinosaur.txt", tmp);
-				1: $readmemb("./imageConverter/cactus.txt", tmp);
-			endcase
+module readImage(clock, image);
+input clock;
+output reg [`imagecount*`imagewidth*`imageheight-1:0] image;
+reg [`imagewidth-1:0] dinosaur[0:`imageheight-1];
+reg [`imagewidth-1:0] cactus[0:`imageheight-1];
+integer i,k;
 
-			for(k=0; k < `imageheight; k=k+1)
-			begin
-				image[i*`imageheight + k] = tmp[k];
-			end
-		end
+initial
+begin
+	for(i=0; i < `imagecount; i=i+1)
+	begin
+		case(i)
+			0: $readmemb("./imageConverter/dinosaur.txt", dinosaur);
+			1: $readmemb("./imageConverter/cactus.txt", cactus);
+		endcase
 	end
-endtask
+	i = 0;
+end
+
+
+always@(posedge clock)
+begin
+	for(k=0; k < `imageheight; k=k+1)
+	begin
+		case(i)
+			0: image[i*`imagewidth*`imageheight + k*`imagewidth +: `imagewidth] = dinosaur[k];
+			1: image[i*`imagewidth*`imageheight + k*`imagewidth +: `imagewidth] = cactus[k];
+		endcase
+	end
+	i = (i+1) % `imagecount;
+end
+
+endmodule 
